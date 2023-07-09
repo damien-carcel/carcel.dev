1539a1231a3e1638619e3ee31d7ab007a52c2155
Damien Carcel <damien.carcel@gmail.com>
2023-07-09 14:25:30 +0200

    Update Remix.run stack
    - Update all dependencies
    - Update Remix files according to latest practices

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import type { LinksFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
 import rootStylesheetUrl from './root.css';
@@ -7,16 +7,12 @@ export const links: LinksFunction = () => {
     return [{ rel: 'stylesheet', href: rootStylesheetUrl }];
 };
 
-export const meta: MetaFunction = () => ({
-    charset: 'utf-8',
-    title: "Hello, I'm Damien",
-    viewport: 'width=device-width,initial-scale=1',
-});
-
 export default function App() {
     return (
         <html lang="en">
             <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
                 <Meta />
                 <Links />
             </head>
