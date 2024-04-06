1957e7985ce28b1cd6a93c0cf2cc0549d81f0e18
Damien Carcel <damien.carcel@gmail.com>
2024-04-06 14:09:11 +0200

    tests(#48): add snapshot tests on "about" and "CV" pages

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,8 +1,20 @@
 import { render } from '@testing-library/react';
 
+import About from '@/app/about/page';
 import Home from '@/app/page';
+import Cv from '@/app/cv/page';
 
 it('renders homepage', () => {
   const { container } = render(<Home />);
+  expect(container).toMatchSnapshot();
+});
+
+it('renders about page', () => {
+  const { container } = render(<About />);
+  expect(container).toMatchSnapshot();
+});
+
+it('renders CV page', () => {
+  const { container } = render(<Cv />);
   expect(container).toMatchSnapshot();
 });
